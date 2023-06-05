b713d63edd772bcdaddf3d2e81f31a11b7da0720
bernstna <130489072+bernstna@users.noreply.github.com>
2023-06-05 00:54:41 -0700

    Inputted Placeholders

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -49,29 +49,37 @@ export class Home extends React.Component {
               <div class = "recipes">
                 <div class = "recipe-header">
                   <div class = "recipe-title">
-                    <h1>Example Title</h1>
+                    <h1>Pulled Pork</h1>
                   </div>
                   <div class = "recipe-category">
-                    <h3>Example Category</h3>
+                    <h3>BBQ</h3>
                   </div>
                 </div>
                 <div class = "recipe-image">
+                  <img src="pulled_pork.png" alt="Delicious Pulled Pork"></img>
                 </div>
                 <div class = "ingredients-list">
                   <ul>
+                    <li>Pork Shoulder</li>
                   </ul>
                 </div>
                 <div class = "recipe-notes">
-
+                  <p>This is a delicious recipe, made by yours truly</p>
                 </div>
                 <div class = "recipe-cookware">
-
+                  <ul>
+                    <li>Smoker</li>
+                  </ul>
                 </div>
                 <div class = "cook-prep-time">
-                  
+                  <p>8 hours 30 min</p>
                 </div>
                 <div class = "procedure">
-
+                  <ul>
+                    <li>I ain't fillin all this out rn</li>
+                    <li>I'm lazy pt2</li>
+                    <li>aaaaaaanddddd pt3</li>
+                  </ul>
                 </div>
               </div>
             </div>
