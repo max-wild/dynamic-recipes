9a5c0abf087a9b4c844d619dc7293b8b372a9dc4
Max Wild <64171062+max-wild@users.noreply.github.com>
2023-06-08 00:16:48 -0700

    Added recipe creation

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-var express = require("express");
+/**
+ * Setup
+ */
+const express = require("express");
 const cors = require("cors");
-var fs = require('fs');
+const fs = require('fs');
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+/**
+ * Recipe HTTP Requests
+ */
 app.get("/recipes", (request, response) => {
     var pdat = [];
     fs.readdirSync("./serverFiles/recipes/").forEach(element => {
@@ -24,6 +32,7 @@ app.get("/recipe/:id", (request, response) => {
     fs.readFile(`./serverFiles/recipes/${request.params.id}.json`, "utf-8", (err, data) => {
         if (err) {
             console.error(err);
+            response.sendStatus(400);
             return;
         }
         response.status(200);
@@ -34,10 +43,48 @@ app.get("/recipe/:id", (request, response) => {
     });
 });
 
+// Create a new recipe
+// Send in a request where the request body has recipe information
+app.post("/recipe", (request, response) => {
+   
+    // Find a valid filename
+    var fileBasename = request.body.name;
+    var fileBasenameSuffix = ""
+    var numberSuffix = 0;
+    if(!fileBasename){
+        fileBasename = "unnamed";
+    }
+    var filepath = path.join("serverFiles", "recipes", `${fileBasename}${fileBasenameSuffix}.json`)
+    while(fs.existsSync(filepath)){
+        // Stops from overwriting a pre-existing file
+        numberSuffix += 1;
+        fileBasenameSuffix = String(numberSuffix);
+        filepath = path.join("serverFiles", "recipes", `${fileBasename}${fileBasenameSuffix}.json`);
+    }
+
+    fs.writeFile(filepath, JSON.stringify(request.body, null, "\t"), err => {
+        if (err) {
+            console.error(err);
+            response.sendStatus(400);
+            return;
+        }
+        response.status(200);
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.type('text/plain');
+        response.send("Success");
+        return;
+    });
+});
+
+
+/**
+ * Shopping HTTP Requests
+ */
 app.get("/shopping", (request, response) => {
     fs.readFile("./serverFiles/shoppingList.json", "utf-8", (err, data) => {
         if (err) {
             console.error(err);
+            response.sendStatus(400);
             return;
         }
         response.status(200);
@@ -50,9 +97,10 @@ app.get("/shopping", (request, response) => {
 
 app.post("/shopping", (request, response) => {
     console.log(request.body);
-    fs.writeFile("./serverFiles/shoppingList.json", JSON.stringify(request.body), err => {
+    fs.writeFile("./serverFiles/shoppingList.json", JSON.stringify(request.body, null, "\t"), err => {
         if (err) {
             console.error(err);
+            response.sendStatus(400);
             return;
         }
         response.status(200);
@@ -63,6 +111,10 @@ app.post("/shopping", (request, response) => {
     });
 });
 
+
+/**
+ * Listener
+ */
 app.listen(3001, () => {
     console.log("Server is running on http://localhost:3001");
 });
